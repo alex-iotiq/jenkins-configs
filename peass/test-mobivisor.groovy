78f013ca7809bacb440741322015f64d985f0530
alex-iotiq <72916554+alex-iotiq@users.noreply.github.com>
2021-09-06 12:41:36 +0200

    added pipeline for mobivisor peass testing

---
+++ peass/test-mobivisor.groovy
@@ -0,0 +1,43 @@
+pipeline {
+    agent any
+    stages {
+        // for clean clone
+        stage('remove current project'){
+            steps{
+                sh 'rm -rf ${JENKINS_HOME}/workspace/${JOB_NAME}/*'
+                sh 'rm -rf ${JENKINS_HOME}/workspace/${JOB_NAME}/.git*'
+            }
+        }
+        // {MOBIVISOR_GITLAB} = path to AndroidMdmAgent project
+        stage('clone'){
+            steps{
+                git branch: 'master', url: '${MOBIVISOR_GITLAB}', credentialsId: 'bormdm'
+            }
+        }
+        // for clean rerun
+        // or use "Clean Peass-CI Cache" on the job page
+        stage('delete peass results'){
+            steps{
+                script{
+                    sh 'rm -rf ${JENKINS_HOME}/jobs/${JOB_NAME}/peass-data'
+                    sh '''temp_job_folder="${JENKINS_HOME}/workspace/${JOB_NAME}_fullPeass"
+                    if [ -d $temp_job_folder ]
+                    	then
+                        	echo "removing ${temp_job_folder}"
+                            rm -rf $temp_job_folder
+                        else
+                        	echo "no ${temp_job_folder} yet"
+                    fi'''
+                }
+            }
+        }
+        stage('performance test') {
+            // it's possible to generate the pipeline script with the "pipeline syntax" link under the pipeline script -> sample step: Measure: Versionsperformance messen
+            // this is the fastest configuration possible, for more accurate measurements you need more VMs / iterations / repetitions / warmup
+            // with includes it's possible to cherry pick the tests to make it faster
+            steps {
+              measure VMs: 2, generateCoverageSelection: true, createDefaultConstructor: false, includes: 'com.boryazilim.android.bormdm.app.test.LogTest#testVerbose', iterations: 1, measurementMode: 'COMPLETE', repetitions: 1, testGoal: 'testBorDebugUnitTest', timeout: 10, updateSnapshotDependencies: false, useSampling: false, useSourceInstrumentation: false, warmup: 1
+            }
+        }
+    }
+}
