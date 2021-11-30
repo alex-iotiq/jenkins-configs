5a2fa9b478c387d664bb170bcc5bdb03d0e93e90
alex-iotiq <72916554+alex-iotiq@users.noreply.github.com>
2021-11-30 17:06:16 +0100

    Update test-mobivisor.groovy

--- peass/test-mobivisor.groovy
+++ peass/test-mobivisor.groovy
@@ -21,25 +21,28 @@ pipeline {
         }
         // for clean rerun
         // or use "Clean Peass-CI Cache" on the job page
-        stage('delete peass results'){
-            steps{
-                script{
-                    sh 'rm -rf ${JENKINS_HOME}/jobs/${JOB_NAME}/peass-data'
-                    sh '''temp_job_folder="${JENKINS_HOME}/workspace/${JOB_NAME}_fullPeass"
-                    if [ -d $temp_job_folder ]
-                    	then
-                        	echo "removing ${temp_job_folder}"
-                            rm -rf $temp_job_folder
-                        else
-                        	echo "no ${temp_job_folder} yet"
-                    fi'''
-                }
-            }
-        }
+//         stage('delete peass results'){
+//             steps{
+//                 script{
+//                     sh 'rm -rf ${JENKINS_HOME}/jobs/${JOB_NAME}/peass-data'
+//                     sh '''temp_job_folder="${JENKINS_HOME}/workspace/${JOB_NAME}_fullPeass"
+//                     if [ -d $temp_job_folder ]
+//                     	then
+//                         	echo "removing ${temp_job_folder}"
+//                             rm -rf $temp_job_folder
+//                         else
+//                         	echo "no ${temp_job_folder} yet"
+//                     fi'''
+//                 }
+//             }
+//         }
         stage('filter passing tests'){
             steps{
                 script{
-                    sh 'git -c sequence.editor="sed -i \'\'" rebase -i "HEAD~2" --exec "cp -fr ${JENKINS_HOME}/peass_scripts/build.gradle ${WORKSPACE}/app && python3 ${JENKINS_HOME}/peass_scripts/find_unit_tests.py ${WORKSPACE}/app/src && git add ${WORKSPACE}/app/build.gradle ${WORKSPACE}/app/src/test/* && git commit --amend --no-edit --allow-empty" -X "theirs"'
+                    sh 'cp -f ${JENKINS_HOME}/peass_scripts/build.gradle ${WORKSPACE}/app'
+                    sh 'python3 ${JENKINS_HOME}/peass_scripts/find_unit_tests.py ${WORKSPACE}/app/src'
+                    sh 'git stash'
+                    sh 'git -c sequence.editor="sed -i \'\'" rebase -i "HEAD~2" --exec "cp -fr ${JENKINS_HOME}/peass_scripts/build.gradle ${WORKSPACE}/app && git add ${WORKSPACE}/app/build.gradle ${WORKSPACE}/app/src/test/* && git commit --amend --no-edit --allow-empty" -X "theirs"'
 //                    sh 'sh ${JENKINS_HOME}/peass_scripts/change_history.sh 2 ${WORKSPACE}/app/src'
 //                     sh 'python3 ${JENKINS_HOME}/peass_scripts/find_unit_tests.py "${WORKSPACE}/app/src"'
 //                     sh 'sh ${JENKINS_HOME}/peass_scripts/git_edit_setup.sh'
@@ -52,7 +55,7 @@ pipeline {
             // this is the fastest configuration possible, for more accurate measurements you need more VMs / iterations / repetitions / warmup
             // with includes it's possible to cherry pick the tests to make it faster
             steps {
-              measure VMs: 2, createDefaultConstructor: false, iterations: 1, measurementMode: 'COMPLETE', nightlyBuild: false, redirectSubprocessOutputToFile: false, redirectToNull: false, repetitions: 1, testGoal: 'testBorDebugUnitTest', timeout: 300, updateSnapshotDependencies: false, useAggregation: false, useSourceInstrumentation: false, versionDiff: 2
+              measure VMs: 2, createDefaultConstructor: false,  generateCoverageSelection: true, iterations: 1, measurementMode: 'COMPLETE', nightlyBuild: false, redirectSubprocessOutputToFile: false, redirectToNull: false, repetitions: 1, showStart: true, testGoal: 'testBorDebugUnitTest', timeout: 300, updateSnapshotDependencies: true
             }
         }
     }
